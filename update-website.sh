cdnjs/buildScript

commit 3125ff3678f5fb3a8e14815b314f88e6b86d762d

check api key first in this script

update-website.sh
@@ -74,7 +74,11 @@ if [ "$updateMeta" = true ]; then
     git push heroku2 meta:master -f
     echo "Now rebuild algolia search index"
     git checkout meta
-    GITHUB_OAUTH_TOKEN=$githubToken ALGOLIA_API_KEY=$algoliaToken node reindex.js
+    if [ -z "$githubToken" ] || [ -z "$algoliaToken" ]; then
+        GITHUB_OAUTH_TOKEN=$githubToken ALGOLIA_API_KEY=$algoliaToken node reindex.js
+    else
+        echo "Missing GitHub or algolia api key, cannot rebuild the searching index"
+    fi
 elif [ "$updateRepo" = true ]; then
     echo "Now push and reploy website only, no need to deploy api due to meta data no update"
     git push heroku meta:master -f

commit 148a56bb15890b732690129721fd111d0b125e48

fix wrong condition on api key checking, caused in 3125ff36

update-website.sh
@@ -103,7 +103,7 @@ if [ "$updateMeta" = true ]; then
         fi
     done
     git checkout meta || Red "Error"
-    if [ -z "$githubToken" ] || [ -z "$algoliaToken" ]; then
+    if [ ! -z "$githubToken" ] && [ ! -z "$algoliaToken" ]; then
         msg="Now rebuild algolia search index"
         Green "$msg"
         gitter "$msg"