2aee9ba5da70cf3556bb14c2ec70f50df0597c89
Peter Dave Hello <hsu@peterdavehello.org>
2015-06-09 06:27:14 +0800

    use colorEcho as submodule

--- update-website.sh
+++ update-website.sh
@@ -12,15 +12,15 @@ updateMeta=false
 updateRepo=false
 
 pth="$(dirname $(readlink -f $0))"
-. "$pth/colorOutput.sh"
+. "$pth/colorEcho/dist/colorEcho.sh"
 
 if [ ! -d "$basePath/$mainRepo" ]; then
-    Red "Main repo not found, exit now."
+    echo.Red "Main repo not found, exit now."
     exit
 fi
 
 if [ ! -d "$basePath/$webRepo" ]; then
-    Red "website repo not found, exit now."
+    echo.Red "website repo not found, exit now."
     exit
 fi
 
@@ -34,96 +34,96 @@ function gitter()
 gitter "Start website/api/index building process on PeterDaveHello's server ..."
 
 if [ "$hasLocalRepo" = true ] && [ -d "$basePath" ]; then
-    Green "Exist cdnjs local repo, fetch objects from local branch first"
-    git fetch local || Red "Error"
+    echo.Green "Exist cdnjs local repo, fetch objects from local branch first"
+    git fetch local || echo.Red "Error"
 else
-    Cyan "Local repo not found, will grab object(s) from GitHub"
+    echo.Cyan "Local repo not found, will grab object(s) from GitHub"
 fi
 
-Cyan "Pull cdnjs main repo with rebase from origin(GitHub)"
+echo.Cyan "Pull cdnjs main repo with rebase from origin(GitHub)"
 status=`git pull --rebase origin master`
 
 if [ "$status" = "Current branch master is up to date." ]; then
     msg="Cdnjs main reop is up to date, no need to rebuild";
-    Cyan "$msg"
+    echo.Cyan "$msg"
     gitter "$msg"
 else
     msg="Rebuild meta data phase 1"
-    Green "$msg"
+    echo.Green "$msg"
     gitter "$msg"
-    git -C $basePath/$webRepo checkout meta || Red "Error"
-    node build/packages.json.js || Red "Error"
+    git -C $basePath/$webRepo checkout meta || echo.Red "Error"
+    node build/packages.json.js || echo.Red "Error"
 
     msg="Rebuild meta data phase 2"
-    Green "$msg"
+    echo.Green "$msg"
     gitter "$msg"
-    cd $basePath/$webRepo || Red "Error"
-    node update.js || Red "Error"
+    cd $basePath/$webRepo || echo.Red "Error"
+    node update.js || echo.Red "Error"
 
     msg="Commit meta data update in website repo"
-    Green "$msg"
+    echo.Green "$msg"
     gitter "$msg"
     for file in atom.xml packages.min.json rss.xml sitemap.xml
     do
-        git -C $basePath/$webRepo add public/$file || Red "Error"
+        git -C $basePath/$webRepo add public/$file || echo.Red "Error"
     done
-    git -C $basePath/$webRepo commit --message="meta data" || Red "Error"
+    git -C $basePath/$webRepo commit --message="meta data" || echo.Red "Error"
 
     updateMeta=true
 fi
 
-Cyan "Change directory into website repo and checkout to master branch"
+echo.Cyan "Change directory into website repo and checkout to master branch"
 cd $basePath/$webRepo
-git checkout master || Red "Error"
+git checkout master || echo.Red "Error"
 
-Cyan "Pull website repo with rebase from origin(Repo)"
+echo.Cyan "Pull website repo with rebase from origin(Repo)"
 webstatus=`git pull --rebase`
 if [ "$webstatus" = "Current branch master is up to date." ]; then
     msg="Website master branch up to date, no need to update meta branch's base"
-    Cyan "$msg"
+    echo.Cyan "$msg"
     gitter "$msg"
 else
     msg="Rebase website's meta branch on master"
-    Green "$msg"
+    echo.Green "$msg"
     gitter "$msg"
-    git rebase master meta || Red "Error"
+    git rebase master meta || echo.Red "Error"
     updateRepo=true
 fi
 
 if [ "$updateMeta" = true ]; then
     msg="Now push and deploy website & api"
-    Green "$msg"
+    echo.Green "$msg"
     gitter "$msg"
     for remote in heroku heroku2
     do
         git push $remote meta:master -f
         if [ ! $? -eq 0 ]; then
             msg="Failed deployment on $remote ..."
-            Red "$msg"
+            echo.Red "$msg"
             gitter "$msg"
         fi
     done
-    git push origin meta -f || Red "Error"
-    git checkout meta || Red "Error"
+    git push origin meta -f || echo.Red "Error"
+    git checkout meta || echo.Red "Error"
     if [ ! -z "$githubToken" ] && [ ! -z "$algoliaToken" ]; then
         msg="Now rebuild algolia search index"
-        Green "$msg"
+        echo.Green "$msg"
         gitter "$msg"
-        GITHUB_OAUTH_TOKEN=$githubToken ALGOLIA_API_KEY=$algoliaToken node reindex.js  || Red "Error"
+        GITHUB_OAUTH_TOKEN=$githubToken ALGOLIA_API_KEY=$algoliaToken node reindex.js  || echo.Red "Error"
     else
-        Red "Missing GitHub or algolia api key, cannot rebuild the searching index"
+        echo.Red "Missing GitHub or algolia api key, cannot rebuild the searching index"
     fi
 elif [ "$updateRepo" = true ]; then
     msg="Now push and deploy website only, no need to deploy api due to meta data no update"
-    Green "$msg"
+    echo.Green "$msg"
     gitter "$msg"
-    git push heroku meta:master -f || Red "Error"
+    git push heroku meta:master -f || echo.Red "Error"
 else
     msg="Didn't update anything, no need to push or deploy."
-    Cyan "$msg"
+    echo.Cyan "$msg"
     gitter "$msg"
 fi
 
 msg="Update finished."
-Cyan "$msg"
+echo.Cyan "$msg"
 gitter "$msg"
