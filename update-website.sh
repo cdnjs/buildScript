09801a9ea239d8d6b456c0d9c2d6b4084f6b640a
Peter Dave Hello <hsu@peterdavehello.org>
2015-07-10 19:10:08 +0800

    [refactor] use new function output to integrate output
    Including console output and message to gitter.im

--- update-website.sh
+++ update-website.sh
@@ -7,123 +7,123 @@ set -e
 pth="$(dirname $(readlink -f $0))"
 . "$pth/colorEcho/dist/ColorEcho.bash"
 
+function output()
+{
+    case "$1" in
+        "Warn" )
+            echo.Red "$2"
+        ;;
+        "Success" )
+            echo.Green "$2"
+        ;;
+        "Info" )
+            echo.Cyan "$2"
+        ;;
+    esac
+    if [ ! -z "$3" ] && [ "$3" = "gitter" ]; then
+        curl --silent -d message="[cronjob] $2" "$gitterHook" > /dev/null
+    fi
+}
+
 if [ ! -d "$basePath/$mainRepo" ]; then
-    echo.Red "Main repo not found, exit now."
+    output Warn "Main repo not found, exit now." gitter
     exit 1
 fi
 
 if [ ! -d "$basePath/$webRepo" ]; then
-    echo.Red "website repo not found, exit now."
+    output Warn "website repo not found, exit now." gitter
     exit 1
 fi
 
 cd "$basePath/$mainRepo"
 
-function gitter()
-{
-    curl --silent -d message="[cronjob] $1" "$gitterHook" > /dev/null
-}
-
-gitter "Start website/api/index building process on PeterDaveHello's server ..."
+output Info "Start website/api/index building process on PeterDaveHello's server ..." gitter
 
 if [ "$hasLocalRepo" = true ] && [ -d "$basePath" ]; then
-    echo.Green "Exist cdnjs local repo, fetch objects from local branch first"
-    git fetch local || echo.Red "Error"
+    output Success "Exist cdnjs local repo, fetch objects from local branch first"
+    git fetch local || output Warn "Error" gitter
 else
-    echo.Cyan "Local repo not found, will grab object(s) from GitHub"
+    output Info "Local repo not found, will grab object(s) from GitHub"
 fi
 
-echo.Cyan "Pull cdnjs main repo with rebase from origin(GitHub)"
+output Info "Pull cdnjs main repo with rebase from origin(GitHub)"
 status=`git pull --rebase origin master`
 
 if [ "$status" = "Current branch master is up to date." ]; then
     msg="Cdnjs main reop is up to date, no need to rebuild";
-    echo.Cyan "$msg"
-    gitter "$msg"
+    output Info "$msg" gitter
 else
     msg="Make sure npm package dependencies, do npm install"
-    echo.Green "$msg"
-    gitter "$msg"
+    output Success "$msg"
     npm install
     msg="Rebuild meta data phase 1"
-    echo.Green "$msg"
-    gitter "$msg"
-    git -C $basePath/$webRepo checkout meta || echo.Red "Error"
-    node build/packages.json.js || echo.Red "Error"
+    output Success "$msg" gitter
+    git -C $basePath/$webRepo checkout meta || output Warn "Error" gitter
+    node build/packages.json.js || output Warn "Error" gitter
 
     msg="Rebuild meta data phase 2"
-    echo.Green "$msg"
-    gitter "$msg"
-    cd $basePath/$webRepo || echo.Red "Error"
-    node update.js || echo.Red "Error"
+    output Success "$msg" gitter
+    cd $basePath/$webRepo || output Warn "Error" gitter
+    node update.js || output Warn "Error" gitter
 
     msg="Commit meta data update in website repo"
-    echo.Green "$msg"
-    gitter "$msg"
+    output Success "$msg" gitter
     for file in atom.xml packages.min.json rss.xml sitemap.xml
     do
-        git -C $basePath/$webRepo add public/$file || echo.Red "Error"
+        git -C $basePath/$webRepo add public/$file || output Warn "Error" gitter
     done
-    git -C $basePath/$webRepo commit --message="meta data" || echo.Red "Error"
+    git -C $basePath/$webRepo commit --message="meta data" || output Warn "Error" gitter
 
     updateMeta=true
 fi
 
-echo.Cyan "Change directory into website repo and checkout to master branch"
+output Info "Change directory into website repo and checkout to master branch"
 cd $basePath/$webRepo
-git checkout master || echo.Red "Error"
+git checkout master || output Warn "Error" gitter
 
-echo.Cyan "Pull website repo with rebase from origin(Repo)"
+output Info "Pull website repo with rebase from origin(Repo)"
 webstatus=`git pull --rebase`
 if [ "$webstatus" = "Current branch master is up to date." ]; then
     msg="Website master branch up to date, no need to update meta branch's base"
-    echo.Cyan "$msg"
-    gitter "$msg"
+    output Info "$msg" gitter
 else
     msg="Make sure npm package dependencies, do npm install"
-    echo.Green "$msg"
+    output Success "$msg" gitter
     npm install
     msg="Rebase website's meta branch on master"
-    echo.Green "$msg"
-    gitter "$msg"
-    git rebase master meta || echo.Red "Error"
+    output Success "$msg" gitter
+    git rebase master meta || output Warn "Error" gitter
     updateRepo=true
 fi
 
 if [ "$updateMeta" = true ]; then
     msg="Now push and deploy website & api"
-    echo.Green "$msg"
-    gitter "$msg"
+    output Success "$msg" gitter
     for remote in heroku heroku2
     do
         git push $remote meta:master -f
         if [ ! $? -eq 0 ]; then
             msg="Failed deployment on $remote ..."
-            echo.Red "$msg"
-            gitter "$msg"
+            output Warn "$msg" gitter
         fi
     done
-    git push origin meta -f || echo.Red "Error"
-    git checkout meta || echo.Red "Error"
+    git push origin meta -f || output Warn "Error" gitter
+    git checkout meta || output Warn "Error" gitter
     if [ ! -z "$githubToken" ] && [ ! -z "$algoliaToken" ]; then
         msg="Now rebuild algolia search index"
-        echo.Green "$msg"
-        gitter "$msg"
-        GITHUB_OAUTH_TOKEN=$githubToken ALGOLIA_API_KEY=$algoliaToken node reindex.js  || echo.Red "Error"
+        output Success "$msg" gitter
+        GITHUB_OAUTH_TOKEN=$githubToken ALGOLIA_API_KEY=$algoliaToken node reindex.js  || output Warn "Error" gitter
     else
-        echo.Red "Missing GitHub or algolia api key, cannot rebuild the searching index"
+        output Warn "Missing GitHub or algolia api key, cannot rebuild the searching index"
     fi
 elif [ "$updateRepo" = true ]; then
     msg="Now push and deploy website only, no need to deploy api due to meta data no update"
-    echo.Green "$msg"
-    gitter "$msg"
-    git push heroku meta:master -f || echo.Red "Error"
+    output Success "$msg" gitter
+    git push heroku meta:master -f || output Warn "Error" gitter
 else
     msg="Didn't update anything, no need to push or deploy."
-    echo.Cyan "$msg"
-    gitter "$msg"
+    output Info "$msg" gitter
 fi
 
 msg="Update finished."
-echo.Cyan "$msg"
-gitter "$msg"
+output Info "$msg" gitter
