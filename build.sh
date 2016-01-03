747dff9d358832eaec84336c997cfce62f05117b
Peter Dave Hello <hsu@peterdavehello.org>
2016-01-03 17:22:50 +0800

    use `gsed` but not `sed` for FreeBSD/Mac, fix #21

--- build.sh
+++ build.sh
@@ -8,12 +8,18 @@ pth="$(dirname $(readlink -f $0))"
 . "$pth/config.sh"
 export PATH="$path:$PATH"
 
+if [ "`uname`" = "FreeBSD" ] || [ "`uname`" = "Darwin" ]; then
+    sed="gsed"
+else
+    sed="sed"
+fi
+
 apiUrl='https://api.github.com/repos/cdnjs/cdnjs/issues'
 
 IssueTitle="[Build failed] Got error while building meta data/artifact"
 IssueAssignee="PeterDaveHello"
 IssueLabels='["Bug - High Priority"]'
-IssueContent="`sed ':a;N;$!ba;s/\n/\\\n/g' $pth/issueTemplate`"
+IssueContent="`$sed ':a;N;$!ba;s/\n/\\\n/g' $pth/issueTemplate`"
 
 Issue="{ \"title\": \"$IssueTitle\", \"body\": \"$IssueContent\", \"assignee\": \"$IssueAssignee\", \"labels\": $IssueLabels }"
 
