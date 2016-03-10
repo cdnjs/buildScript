cdnjs/buildScript

commit 63cf9dbd132f8b7655e0e70832b1d1364e92976f

add NVM_BIN to PATH if it exists, fix #36

update-website.sh
@@ -8,6 +8,10 @@ function init()
     updateMeta=$forceUpdateMeta
     updateRepo=$forceUpdateRepo
 
+    if [[ ! -z "$NVM_BIN" ]];
+        path="$NVM_BIN:$path"
+    fi
+
     export PATH="$path:$PATH"
 
     if [[ ! $timeout =~ ^[0-9]+$ ]] || [ $timeout -le 3 ]; then

commit c323c713434409586a6954b03fc472fedf3901a1

fix syntax error caused in 63cf9db

update-website.sh
@@ -8,7 +8,7 @@ function init()
     updateMeta=$forceUpdateMeta
     updateRepo=$forceUpdateRepo
 
-    if [[ ! -z "$NVM_BIN" ]];
+    if [[ ! -z "$NVM_BIN" ]]; then
         path="$NVM_BIN:$path"
     fi
 