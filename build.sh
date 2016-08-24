cdnjs/buildScript

commit 9de160606d39bbb0e9f26b889e5ceb3931a70c2a

refactor build.sh

build.sh
@@ -2,13 +2,15 @@
 
 # Sciprt for cronjob, will open an issue on cdnjs/cdnjs if build failed.
 
-StartTimestamp="`date +%s`"
+StartTimestamp="$(date +%s)"
 
-pth="$(dirname $(readlink -f $0))"
+pth="$(dirname "$(readlink -f "$0")")"
+
 . "$pth/config.sh"
+
 export PATH="$path:$PATH"
 
-if [ "`uname`" = "FreeBSD" ] || [ "`uname`" = "Darwin" ]; then
+if [ "$(uname)" = "FreeBSD" ] || [ "$(uname)" = "Darwin" ]; then
     sed="gsed"
 else
     sed="sed"
@@ -19,17 +21,17 @@ apiUrl='https://api.github.com/repos/cdnjs/cdnjs/issues'
 IssueTitle="[Build failed] Got error while building meta data/artifact"
 IssueAssignee="PeterDaveHello"
 IssueLabels='["Bug - High Priority"]'
-IssueContent="`$sed ':a;N;$!ba;s/\n/\\\n/g' $pth/issueTemplate`"
+IssueContent="$($sed ':a;N;$!ba;s/\n/\\n/g' "$pth/issueTemplate"))"
 
 Issue="{ \"title\": \"$IssueTitle\", \"body\": \"$IssueContent\", \"assignee\": \"$IssueAssignee\", \"labels\": $IssueLabels }"
 
-$pth/update-website.sh build
+"$pth/update-website.sh" build
 
 error=$?
 
 [[ $error -ne 0 ]] && curl --silent -H "Authorization: token $githubToken" -d "$Issue" "$apiUrl" > /dev/null
 
-EndTimestamp="`date +%s`"
+EndTimestamp="$(date +%s)"
 
-echo -e "\nTotal time spent for this build is _$(($EndTimestamp - $StartTimestamp))_ second(s)\n"
+echo -e "\nTotal time spent for this build is _$((EndTimestamp - StartTimestamp))_ second(s)\n"
 exit $error

commit 2b4d3bf3e7752dac73a8dc4f1b1c8118edbeb732

fix typo caused in 9de1606(refactor build.sh)

build.sh
@@ -21,7 +21,7 @@ apiUrl='https://api.github.com/repos/cdnjs/cdnjs/issues'
 IssueTitle="[Build failed] Got error while building meta data/artifact"
 IssueAssignee="PeterDaveHello"
 IssueLabels='["Bug - High Priority"]'
-IssueContent="$($sed ':a;N;$!ba;s/\n/\\n/g' "$pth/issueTemplate"))"
+IssueContent="$($sed ':a;N;$!ba;s/\n/\\n/g' "$pth/issueTemplate")"
 
 Issue="{ \"title\": \"$IssueTitle\", \"body\": \"$IssueContent\", \"assignee\": \"$IssueAssignee\", \"labels\": $IssueLabels }"
 